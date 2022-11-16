c8752b893d75e21e65cc3ca207fb1591903dbea6
itsaimejia <33639089+itsaimejia@users.noreply.github.com>
2022-11-15 23:03:00 -0800

    estructura pantalla imagenes

--- pages/images.tsx
+++ pages/images.tsx
@@ -1,10 +1,41 @@
+import { Box, Button, Flex, Grid, Group, NativeSelect, Stack } from '@mantine/core'
 import React from 'react'
 import { Layout } from '../components/Layout'
+import { LayoutBody } from '../components/LayoutBody'
 
 export default function Images() {
     return (
         <Layout>
-            Images
+            <LayoutBody titlePage='Imagenes'>
+                <Button sx={{ width: 200 }}>Seleccionar archivo</Button>
+                <Flex
+                    mih={50}
+                    bg="white"
+                    gap="xs"
+                    justify="flex-start"
+                    align="flex-start"
+                    direction="row"
+                    wrap="nowrap"
+                >
+                    <Box sx={{ backgroundColor: 'grey', height: 650, width: '80%' }}>
+                    </Box>
+                    <Stack sx={{ backgroundColor: 'white', height: 650, padding: 10, width: '20%' }}>
+                        <NativeSelect
+                            sx={{}}
+                            data={['React', 'Vue', 'Angular', 'Svelte']}
+                        />
+                    </Stack>
+
+                </Flex>
+                <Flex
+                    justify="center"
+                    align="center"
+                >
+                    <Button sx={{ width: 250, alignContent: 'center' }}>Subir imagen</Button>
+                </Flex>
+
+            </LayoutBody>
         </Layout>
     )
 }
+
