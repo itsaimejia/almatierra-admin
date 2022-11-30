4db7178d02c3d7b10e65b3385c0c5e2d6380a51f
itsaimejia <33639089+itsaimejia@users.noreply.github.com>
2022-11-29 17:06:26 -0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthContextProvider } from '../context/AuthContext'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <AuthContextProvider>
-    <Component {...pageProps} />
-  </AuthContextProvider>
+  return (
+    <>
+      <Head>
+        <title>Menu Almatierra - Admin</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <AuthContextProvider>
+        <Component {...pageProps} />
+      </AuthContextProvider>
+    </>
+  )
 }
